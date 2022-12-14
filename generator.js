431c2aad7e1e753ea2468a56cd5279ea90b24b8e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-14 04:42:04 -0800

    first try

---
+++ generator.js
@@ -0,0 +1,4 @@
+const phraseBank={
+    familyMemeber = ['momma','daddy','sister','brother','dog','aunt','cow'],
+    Insult = ["so ugly", "so smelly", 'so fat', 'so ugly', 'so poor'],
+    reasonFat = ["makes little kids cry","when she fell I didn't laugh, but the sidewalk cracked up.","when she skips a meal, the stock market drops.","it took me two buses and a train to get to her good side.","when she goes camping, the bears hide their food."]
