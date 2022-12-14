0697f61ce52e551860ebd79e1c7d5c9f5b9ec2aa
BuildTools <unconfigured@null.spigotmc.org>
2022-12-14 04:47:36 -0800

    dsfsdfasd sad asd fas dfasd f asdf asdf asdf asdf'gsdfkghl;sdkfjhg SADOJF aojfa 'sdklhjg AF ladshjgf ;SKLDHGB PEDOIRGP L;GKJDSFL;GKJSDF;LGKJSDFG SDFG SJDFG S;DLFJG SD;LFJKGL ;KFDJG AERJG AERAJGE PTRIJDF B; FBSA DAE RDFG;JDF;GLKJHEROIG0TBNMFG DFFD DF EFDRG/GB/B FDFDGB

--- generator.js
+++ generator.js
@@ -2,3 +2,10 @@ const phraseBank={
     familyMemeber = ['momma','daddy','sister','brother','dog','aunt','cow'],
     Insult = ["so ugly", "so smelly", 'so fat', 'so ugly', 'so poor'],
     reasonFat = ["makes little kids cry","when she fell I didn't laugh, but the sidewalk cracked up.","when she skips a meal, the stock market drops.","it took me two buses and a train to get to her good side.","when she goes camping, the bears hide their food."]
+
+    /* to Save time i would have a function that would generate random number = len of the array,
+     first it would choose 3 random numbers then attach the first to the family member,
+      then the second an insult,
+       then depending on the insult it would use the final random number to decide from the correct array of reasonXxxx.
+    */
+   
